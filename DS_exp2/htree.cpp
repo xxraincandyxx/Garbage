7187da23d880d66bb229273a1e9f0615003a9d5d
rain <xxraincandyxx@gamail.com>
2024-11-07 19:51:32 +0800

    Update Huffman Tree

--- DS_exp2/htree.cpp
+++ DS_exp2/htree.cpp
@@ -9,19 +9,42 @@
 
 struct hfNode {
     char ch;
-    int weight;
+    int freq;
     hfNode *left;
     hfNode *right;
 
-    hfNode(char ch, int weight) : ch(ch), weight(weight), left(nullptr), right(nullptr) {}
+    hfNode(char ch, int freq) : ch(ch), freq(freq), left(nullptr), right(nullptr) {}
 };
 
 class HuffmanTree {
 public:
     HuffmanTree() : root(nullptr) {}
 
-    void build(const std::unordered_map<char, int>& weight_map) {
-        root = build_huffmantree(weight_map);
+    void build(const std::unordered_map<char, int>& freq_map) {
+        root = build_huffmantree(freq_map);
+        build_codemap(root, "", codemap);
+    }
+
+    std::string encode(const std::string& text) {
+        if (codemap.empty() || !root) { return ""; }
+        return encoder(text);
+    }
+
+    std::string decode(const std::string& code) {
+        if (codemap.empty() || !root) { return ""; }
+        std::string text = "";
+        decoder(root, code, text, 0);
+        return text;
+    }
+
+    void output_codemap() {
+        if (codemap.empty()) {
+            std::cout << "None" << std::endl;
+            return;
+        }
+        for (const auto& [ch, str] : codemap) {
+            std::cout << "Character: " << ch << " Code: " << str << std::endl;
+        }
     }
 
     void preorder() const {
@@ -58,11 +81,12 @@ public:
 
 private:
     hfNode *root;
+    std::unordered_map<char, std::string> codemap;
 
     void preorder_traversal(hfNode* node) const {
         if (node == nullptr) { return; }
         std::cout << "Character: " << node->ch << ' ' << std::endl;
-        std::cout << "Weight: " << node->weight << std::endl << std::endl;
+        std::cout << "freq: " << node->freq << std::endl << std::endl;
         preorder_traversal(node->left);
         preorder_traversal(node->right);
     }
@@ -71,7 +95,7 @@ private:
         if (node == nullptr) { return; }
         inorder_traversal(node->left);
         std::cout << "Character: " << node->ch << ' ' << std::endl;
-        std::cout << "Weight: " << node->weight << std::endl << std::endl;
+        std::cout << "freq: " << node->freq << std::endl << std::endl;
         inorder_traversal(node->right);
     }
 
@@ -80,7 +104,7 @@ private:
         postorder_traversal(node->left);
         postorder_traversal(node->right);
         std::cout << "Character: " << node->ch << ' ' << std::endl;
-        std::cout << "Weight: " << node->weight << std::endl << std::endl;
+        std::cout << "freq: " << node->freq << std::endl << std::endl;
     }
 
     void rm_branch(hfNode* node) {
@@ -90,46 +114,73 @@ private:
         delete node;
     }
 
-    void build_huffmancode(const hfNode* root, std::string str, std::unordered_map<char, std::string> &code_map) {
-        if (root == nullptr) { return; }
-        if (root->left == nullptr && root->right == nullptr) {
-            code_map[root->ch] = str;
+    void build_codemap(const hfNode* node, std::string str, std::unordered_map<char, std::string>& code_map) {
+        if (node == nullptr) { return; }
+        if (node->left == nullptr && node->right == nullptr) {
+            code_map[node->ch] = str;
         } else {
-            build_huffmancode(root->left, str + "0", code_map);
-            build_huffmancode(root->right, str + "1", code_map);
+            build_codemap(node->left, str + "0", code_map);
+            build_codemap(node->right, str + "1", code_map);
         }
     }
 
-    hfNode* build_huffmantree(const std::unordered_map<char, int>& weight_map) {
-        auto comp = [](const hfNode* a, const hfNode* b) { return a->weight > b->weight; };
+    hfNode* build_huffmantree(const std::unordered_map<char, int>& freq_map) {
+        auto comp = [](const hfNode* a, const hfNode* b) { return a->freq > b->freq; };
         std::priority_queue<hfNode*, std::vector<hfNode*>, decltype(comp)> minHeap(comp);
-        for (const auto& [ch, weight] : weight_map) {
-            minHeap.push(new hfNode(ch, weight));
+        for (const auto& [ch, freq] : freq_map) {
+            minHeap.push(new hfNode(ch, freq));
         }
 
         while (minHeap.size() > 1) {
             auto left = minHeap.top(); minHeap.pop();
             auto right = minHeap.top(); minHeap.pop();
 
-            auto sum = new hfNode('\0', left->weight + right->weight);
+            auto sum = new hfNode('\0', left->freq + right->freq);
             sum->left = left;
             sum->right = right;
             minHeap.push(sum);
         }
         return minHeap.top();
     }
+
+    std::string encoder(std::string text) {
+        if (codemap.empty() || !root) { return ""; }
+        std::string code = "";
+        for (char ch : text) { code += codemap[ch]; }
+        return code;
+    }
+
+    void decoder(const hfNode* node, const std::string& code, std::string& text, int cur) {
+        if (node->left == nullptr && node->right == nullptr) {
+            text += node->ch;
+            decoder(root, code, text, cur);
+            return;
+        }
+        if (code[cur] == '\0') { return; }        
+        if (code[cur] == '0') { decoder(node->left, code, text, cur + 1); }
+        else if (code[cur] == '1') { decoder(node->right, code, text, cur + 1); }
+        return;
+    }
 };
 
 int main(void) {
     HuffmanTree root;
-    std::unordered_map<char, int> weights_map = {
+    std::unordered_map<char, int> freq_map = {
         {'A', 5}, {'B', 9}, {'C', 12}, {'D', 13}, {'E', 16}, {'F', 45}
     };
 
-    root.build(weights_map);
+    root.build(freq_map);
     root.preorder();
     // root.inorder();
     // root.postorder();
+
+    std::cout << "Codemap:" << std::endl;
+    root.output_codemap();
+    std::string text = "ABCDEF";
+    std::string code = root.encode(text);
+    std::cout << "Encoded: " << code << std::endl;
+    std::cout << "Decoded: " << root.decode(code) << std::endl;
+
     root.destroy();
 
     return 0;
